cb4f880390e2f0b0a4bc3b3fc3dc78b43f63c097
eortizutalca <eortiz@utalca.cl>
2016-05-21 11:51:43 -0400

    Primer commit de la solucion que representa el ajedrez, ademas se adjunta una clase de ejemplo en el proyecto Models

---
+++ Ajedrez/Ajedrez.Consola/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajedrez.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persona = new Models.Persona();
+            Console.WriteLine(persona.Edad);
+        }
+    }
+}
---
+++ Ajedrez/Ajedrez.Models/Ejemplo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajedrez.Models
+{
+    /// <summary>
+    /// Esta es una clase de ejemplo para que vean como se declaran las propiedades, metodos, atributos y clases
+    /// </summary>
+    public class Persona
+    {
+        private int edad = 0; //atributo de la clase
+
+        /// <summary>
+        /// constructor de esta clase
+        /// </summary>
+        public Persona()
+        {
+            this.FechaNacimiento = new DateTime(1985, 12, 1);
+        }
+
+        /// <summary>
+        /// Propiedad automática
+        /// </summary>
+        public DateTime FechaNacimiento
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Propiedad con implementacion
+        /// </summary>
+        public int Edad
+        {
+            get
+            {
+                this.CalcularEdad();
+                return this.edad;
+            }
+        }
+
+
+        /// <summary>
+        /// metodo
+        /// </summary>
+        public void CalcularEdad()
+        {
+            this.edad = Convert.ToInt32((DateTime.Now - this.FechaNacimiento).TotalDays / 365);
+        }
+
+
+    }
+}
