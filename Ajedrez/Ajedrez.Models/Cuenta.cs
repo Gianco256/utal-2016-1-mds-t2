3bcea0f582e558f2ca4dd559c60b688e14debd67
Gianfranco <gianco256@gmail.com>
2016-05-24 14:32:04 -0400

    creadas las entidades Cuenta y Partida

---
+++ Ajedrez/Ajedrez.Models/Cuenta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajedrez.Models
+{
+    class Cuenta
+    {
+        private string email;
+        private string password;
+        private DateTime ultimoAcceso;
+        private Jugador jugadorActual;
+
+        Cuenta(){
+            throw new NotImplementedException();
+        }
+
+        public bool registrar(string email, string password){
+            throw new NotImplementedException();
+        }
+
+        public bool seleccionar(string email){
+            throw new NotImplementedException();
+        }
+
+        public void iniciarSesion(){
+            throw new NotImplementedException();
+        }
+
+        public void cerrarSesion(){
+            throw new NotImplementedException();
+        }
+
+        public List<Jugador> jugadores(){
+            throw new NotImplementedException();
+        }
+
+        public void cambiarJugadorActivo(Jugador jugador){
+            throw new NotImplementedException();
+        }
+
+        public bool crearJugador(jugador){
+            throw new NotImplementedException();
+        }
+
+        public bool eliminarJugador(jugador){
+            throw new NotImplementedException();
+        }
+
+        public void desactivar(){
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ajedrez/Ajedrez.Models/Partida.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajedrez.Models
+{
+    class Partida {
+        private int[,] tablero = new int[8, 8];
+        private Jugador blancas;
+        private Jugador negras;
+        private DateTime inicio;
+        private DateTime ultimaJugada;
+        private bool turno;
+        private List<Jugada> jugadas= new List<Jugada>();
+
+        Partida(){
+            throw new NotImplementedException();
+        }
+
+        public void iniciar(){
+            throw new NotImplementedException();
+        }
+
+        public bool jugar(Jugada jugada){
+            throw new NotImplementedException();
+        }
+
+        private bool validarJugada(Jugada jugada){
+            throw new NotImplementedException();
+        }
+
+        private bool mover(Jugada jugada){
+            throw new NotImplementedException();
+        }
+
+        private void guardarJugada(Jugada jugada){
+            throw new NotImplementedException();
+        }
+
+        private bool validarTablero(){
+            throw new NotImplementedException();
+        }
+
+        private void retroceder(){
+            throw new NotImplementedException();
+        }
+
+        private void cambiarTurno(){
+            throw new NotImplementedException();
+        }
+    }
+}
