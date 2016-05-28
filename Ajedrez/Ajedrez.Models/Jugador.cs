412a930bd8d3a18c63ee8d22a6a2985de99a85ba
CachabachaMarco <marco.espinoza@live.cl>
2016-05-28 18:10:58 -0300

    Falta implementar metodos

--- Ajedrez/Ajedrez.Models/Jugador.cs
+++ Ajedrez/Ajedrez.Models/Jugador.cs
@@ -6,5 +6,24 @@ using System.Threading.Tasks;
 
 namespace Ajedrez.Models {
 	public class Jugador {
+        public long Id { get; set; }
+        public String Nick { get; set; }
+        public Sexo Sexo { get; set;}
+        public DateTime FechaNacimiento { get; set; }
+
+        public List<Partida> Partidas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Desafiar(Partida partida)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Eliminar(Partida partida)
+        {
+            throw new NotImplementedException();
+        }
 	}
 }
