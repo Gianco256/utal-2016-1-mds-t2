a60be4e8ecb45dfc2c81dae193136baf89a5faa1
Vicente Calderón <vicente1995@gmail.com>
2016-05-29 15:02:00 -0400

    Arreglados errores en el proyecto

--- Ajedrez/Ajedrez.Models/Sexo.cs
+++ Ajedrez/Ajedrez.Models/Sexo.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ajedrez.Models
-{
-    public enum Sexo
-    {
-        MASCULINO,
-        FEMENINO
-    }
+namespace Ajedrez.Models {
+	public enum Sexo {
+		MASCULINO,
+		FEMENINO
+	}
 }
