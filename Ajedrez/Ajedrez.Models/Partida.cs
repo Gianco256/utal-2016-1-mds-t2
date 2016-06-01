08a38e405747cc577104160bff82ddc4d55f45e0
Vicente Calderón <vicente1995@gmail.com>
2016-06-01 14:41:46 -0400

    Agregadas implementaciones de métodos a Partida

--- Ajedrez/Ajedrez.Models/Partida.cs
+++ Ajedrez/Ajedrez.Models/Partida.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Xml;
+using System.Xml.Linq;
+
 namespace Ajedrez.Models {
 	public class Partida {
 		private Pieza[,] Tablero;
@@ -33,19 +36,41 @@ namespace Ajedrez.Models {
 		}
 
 		public bool Jugar(Jugada jugada) {
-			throw new NotImplementedException();
+			if (this.ValidarJugada(jugada)) {
+				this.Mover(jugada);
+				this.GuardarJugada(jugada);
+				this.ValidarTablero();
+				return true;
+			}
+			this.Retroceder();
+			this.ValidarTablero(); // No sé si será necesario revalidar el tablero en caso de un movimiento no válido
+			return false;
 		}
 
 		private bool ValidarJugada(Jugada jugada) {
 			throw new NotImplementedException();
 		}
 
-		private bool Mover(Jugada jugada) {
-			throw new NotImplementedException();
+		private void Mover(Jugada jugada) {
+			Pieza temp = this.Tablero[jugada.Origen.Traducir()[0], jugada.Origen.Traducir()[1]];
+			this.Tablero[jugada.Origen.Traducir()[0], jugada.Origen.Traducir()[1]] = null;
+			this.Tablero[jugada.Destino.Traducir()[0], jugada.Destino.Traducir()[1]] = temp;
 		}
 
 		private void GuardarJugada(Jugada jugada) {
-			throw new NotImplementedException();
+			System.IO.Directory.CreateDirectory(@"C:\utal-2016-1-mds-t2");
+			XDocument xDoc;
+			if (!System.IO.File.Exists(@"C:\utal-2016-1-mds-t2\jugadas[ID].xml")) {
+				xDoc = new XDocument(
+					new XDeclaration("1.0", "utf-8", "yes"),
+					new XComment("Lista de jugadas de la partida [ID]"),
+					new XElement("Jugadas")
+					);
+			} else {
+				xDoc = XDocument.Load(@"C:\utal-2016-1-mds-t2\jugadas[ID].xml");
+			}
+			xDoc.Element("Jugadas").Add(new XElement("Jugada", jugada.ToString()));
+			xDoc.Save(@"C:\utal-2016-1-mds-t2\jugadas[ID].xml");
 		}
 
 		private bool ValidarTablero() {
