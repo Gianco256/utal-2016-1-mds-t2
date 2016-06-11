3969de5c014f9a189785a01f86718b8e7d905cb6
Vicente Calderón <vicente1995@gmail.com>
2016-06-11 12:40:44 -0400

    Reformato y cambio a la ruta de destino de los XML

--- Ajedrez/Ajedrez.Models/Cuenta.cs
+++ Ajedrez/Ajedrez.Models/Cuenta.cs
@@ -7,222 +7,185 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 
-namespace Ajedrez.Models
-{
-    public class Cuenta
-    {
+namespace Ajedrez.Models {
+	public class Cuenta {
+		private const string RutaXML = @"C:\Ajedrez";
+		private const string RutaXMLCuentas = @"C:\Ajedrez\Cuentas.xml";
+		private const string RutaXMLJugadores = @"C:\Ajedrez\Jugadores.xml";
 
-        private const string RutaXMLCuentas = @"C:\Users\polux\Desktop\Cuentas.xml";
-        private const string RutaXMLJugadores = @"C:\Users\polux\Desktop\Jugadores.xml";
+		public string Email {
+			get; set;
+		}
+		public string Password {
+			get; set;
+		}
+		public DateTime UltimoAcceso {
+			get; set;
+		}
+		public Jugador JugadorActual {
+			get; set;
+		}
 
+		public Cuenta() {
+		}
 
-        public string Email
-        {
-            get; set;
-        }
-        public string Password
-        {
-            get; set;
-        }
-        public DateTime UltimoAcceso
-        {
-            get; set;
-        }
-        public Jugador JugadorActual
-        {
-            get; set;
-        }
+		public Cuenta(string email, string password, DateTime ultimoAcceso, Jugador jugadorActual) {
+			this.Email = email;
+			this.Password = password;
+			this.UltimoAcceso = ultimoAcceso;
+			this.JugadorActual = jugadorActual;
+		}
 
-        public Cuenta()
-        {
-        }
-
-        public Cuenta(string email, string password, DateTime ultimoAcceso, Jugador jugadorActual)
-        {
-            this.Email = email;
-            this.Password = password;
-            this.UltimoAcceso = ultimoAcceso;
-            this.JugadorActual = jugadorActual;
-        }
-
-        public bool Registrar(string email, string password)
-        {
-            System.IO.Directory.CreateDirectory(@"C:\Users\polux\Desktop");
-            XmlDocument xDoc = new XmlDocument();
-            if (!System.IO.File.Exists(RutaXMLCuentas))
-            {
-                xDoc.LoadXml("<Cuentas></Cuentas>");
-            }
-            else {
-                xDoc.Load(RutaXMLCuentas);
-            }
-            if (!this.Seleccionar(email))
-            {
-                var xmldf = xDoc.CreateDocumentFragment();
-                xmldf.InnerXml = @"<Cuenta>
+		public bool Registrar(string email, string password) {
+			System.IO.Directory.CreateDirectory(RutaXML);
+			XmlDocument xmlDoc = new XmlDocument();
+			if (!System.IO.File.Exists(RutaXMLCuentas)) {
+				xmlDoc.LoadXml("<Cuentas></Cuentas>");
+			} else {
+				xmlDoc.Load(RutaXMLCuentas);
+			}
+			if (!this.Seleccionar(email)) {
+				var xmldf = xmlDoc.CreateDocumentFragment();
+				xmldf.InnerXml = @"<Cuenta>
                         <Email>" + email + @"</Email>
                         <Password>" + password + @"</Password>
                         <UltimoAcceso>" + DateTime.Now.Ticks + @"</UltimoAcceso>
                         <JugadorActual></JugadorActual>
                     </Cuenta>";
-                xDoc.FirstChild.AppendChild(xmldf);
-                xDoc.Save(RutaXMLCuentas);
-                return true;
-            }
-            return false;
-        }
+				xmlDoc.FirstChild.AppendChild(xmldf);
+				xmlDoc.Save(RutaXMLCuentas);
+				return true;
+			}
+			return false;
+		}
 
-        public bool Seleccionar(string email)
-        {
-            try
-            {
-                XmlDocument xDoc = new XmlDocument();
-                xDoc.Load(RutaXMLCuentas);
-                var xCuenta = xDoc.SelectSingleNode("/Cuentas/Cuenta[Email = '" + email + "']");
-                if (xCuenta == null)
-                {
-                    return false;
-                }
-                else {
-                    return true;
-                }
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
+		public bool Seleccionar(string email) {
+			try {
+				XmlDocument xmlDoc = new XmlDocument();
+				xmlDoc.Load(RutaXMLCuentas);
+				var xmlCuenta = xmlDoc.SelectSingleNode("/Cuentas/Cuenta[Email = '" + email + "']");
+				if (xmlCuenta == null) {
+					return false;
+				} else {
+					return true;
+				}
+			} catch (Exception ex) {
+				return false;
+			}
+		}
 
-        public bool IniciarSesion()
-        {
-            XmlDocument xDoc = new XmlDocument();
-            xDoc.Load(RutaXMLCuentas);
-            var xCuenta = xDoc.SelectSingleNode("/Cuentas/Cuenta[Email = '" + this.Email + "' and Password = '" + this.Password + "']");
-            if (xCuenta == null)
-            {
-                return false;
-            }
-            else {
-                this.UltimoAcceso = DateTime.Now;
-                xDoc.Save(RutaXMLCuentas);
-                return true;
-            }
-        }
+		public bool IniciarSesion() {
+			XmlDocument xmlDoc = new XmlDocument();
+			xmlDoc.Load(RutaXMLCuentas);
+			var xmlCuenta = xmlDoc.SelectSingleNode("/Cuentas/Cuenta[Email = '" + this.Email + "' and Password = '" + this.Password + "']");
+			if (xmlCuenta == null) {
+				return false;
+			} else {
+				this.UltimoAcceso = DateTime.Now;
+				xmlDoc.Save(RutaXMLCuentas);
+				return true;
+			}
+		}
 
-        public void CerrarSesion()
-        {
-            throw new NotImplementedException();
-        }
+		public void CerrarSesion() {
+			throw new NotImplementedException();
+		}
 
-        public List<Jugador> Jugadores()
-        {
-            XmlDocument xDoc = new XmlDocument();
-            List<Jugador> acum = new List<Jugador>();
-            if (!System.IO.File.Exists(RutaXMLJugadores)) return acum;
-            else
-                xDoc.Load(RutaXMLJugadores);
+		public List<Jugador> Jugadores() {
+			XmlDocument xmlDoc = new XmlDocument();
+			List<Jugador> acum = new List<Jugador>();
+			if (!System.IO.File.Exists(RutaXMLJugadores))
+				return acum;
+			else
+				xmlDoc.Load(RutaXMLJugadores);
 
-            var entrada = xDoc.SelectNodes("/Jugadores/Jugador[Email='" + Email + "']");
+			var entrada = xmlDoc.SelectNodes("/Jugadores/Jugador[Email='" + Email + "']");
 
-            foreach (XmlNode th in entrada)
-            {
-                acum.Add(new Jugador()
-                {
-                    Id = Int64.Parse(th["Id"].InnerText),
-                    Nick = th["Nick"].InnerText,
-                    Sexo = (Sexo)Convert.ToInt32(th["Sexo"].InnerText),
-                    FechaNacimiento = new DateTime(Int64.Parse(th["FechaNacimiento"].InnerText)),
-                });
-            }
-            return acum;
-        }
+			foreach (XmlNode th in entrada) {
+				acum.Add(new Jugador() {
+					Id = Int64.Parse(th["Id"].InnerText),
+					Nick = th["Nick"].InnerText,
+					Sexo = (Sexo) Convert.ToInt32(th["Sexo"].InnerText),
+					FechaNacimiento = new DateTime(Int64.Parse(th["FechaNacimiento"].InnerText)),
+				});
+			}
+			return acum;
+		}
 
-        public void CambiarJugadorActivo(Jugador jugador)
-        {
-            XmlDocument xDoc = new XmlDocument();
-            if (!System.IO.File.Exists(RutaXMLJugadores)) return;
-            else xDoc.Load(RutaXMLJugadores);
+		public void CambiarJugadorActivo(Jugador jugador) {
+			XmlDocument xmlDoc = new XmlDocument();
+			if (!System.IO.File.Exists(RutaXMLJugadores))
+				return;
+			else
+				xmlDoc.Load(RutaXMLJugadores);
 
-            if (xDoc.SelectSingleNode("/Jugadores/Jugador['Id=" + jugador.Id + " Email=" + this.Email + "]") != null)
-            {
-                xDoc.Load(RutaXMLCuentas);
-                var cuenta = xDoc.SelectSingleNode("//Cuenta[Email='" + this.Email + "']");
-                if(cuenta != null)
-                {
-                    cuenta["JugadorActual"].InnerText = jugador.Id.ToString();
-                    this.JugadorActual = jugador;
-                    xDoc.Save(RutaXMLCuentas);
-                }
-            }
-        }
+			if (xmlDoc.SelectSingleNode("/Jugadores/Jugador['Id=" + jugador.Id + " Email=" + this.Email + "]") != null) {
+				xmlDoc.Load(RutaXMLCuentas);
+				var cuenta = xmlDoc.SelectSingleNode("//Cuenta[Email='" + this.Email + "']");
+				if (cuenta != null) {
+					cuenta["JugadorActual"].InnerText = jugador.Id.ToString();
+					this.JugadorActual = jugador;
+					xmlDoc.Save(RutaXMLCuentas);
+				}
+			}
+		}
 
-        public bool CrearJugador(Jugador jugador)
-        {
-            XmlDocument xDoc = new XmlDocument();
-            if (!System.IO.File.Exists(RutaXMLJugadores))
-            {
-                xDoc.LoadXml("<Jugadores></Jugadores>");
-            }
-            else
-                xDoc.Load(RutaXMLJugadores);
+		public bool CrearJugador(Jugador jugador) {
+			XmlDocument xmlDoc = new XmlDocument();
+			if (!System.IO.File.Exists(RutaXMLJugadores)) {
+				xmlDoc.LoadXml("<Jugadores></Jugadores>");
+			} else
+				xmlDoc.Load(RutaXMLJugadores);
 
-            var xJugador = xDoc.SelectSingleNode("//Jugadores/Jugador[Email='" + this.Email + "']");
-            if (xJugador == null)
-            {
-                var xmldf = xDoc.CreateDocumentFragment();
-                xmldf.InnerXml = @"<Jugador>
+			var xmlJugador = xmlDoc.SelectSingleNode("//Jugadores/Jugador[Email='" + this.Email + "']");
+			if (xmlJugador == null) {
+				var xmldf = xmlDoc.CreateDocumentFragment();
+				xmldf.InnerXml = @"<Jugador>
                     <Email>" + this.Email + @"</Email>
                     <Id>" + jugador.Id + @"</Id>
                     <Nick>" + jugador.Nick + @"</Nick>
                     <Sexo>" + Convert.ToInt32(jugador.Sexo) + @"</Sexo>
                     <FechaNacimiento>" + jugador.FechaNacimiento.Ticks + @"</FechaNacimiento>
                 </Jugador>";
-                xDoc.SelectSingleNode("/Jugadores").AppendChild(xmldf);
-                
-                xDoc.Save(RutaXMLJugadores);
-                return true;
-            }
-            return false;
-        }
+				xmlDoc.SelectSingleNode("/Jugadores").AppendChild(xmldf);
 
-        public bool EliminarJugador(Jugador jugador)
-        {
-            if (!System.IO.File.Exists(RutaXMLJugadores) || jugador == null) return false;
+				xmlDoc.Save(RutaXMLJugadores);
+				return true;
+			}
+			return false;
+		}
 
-            XmlDocument xDoc = new XmlDocument();
-            xDoc.Load(RutaXMLJugadores);
-            var elemjugador = xDoc.SelectSingleNode("/Jugadores/Jugador['Id=" + jugador.Id + " Id-Cuenta=" + this.Email + "]");
-            if (elemjugador != null)
-            {
-                elemjugador.ParentNode.RemoveChild(elemjugador);
-                xDoc.Save(RutaXMLJugadores);
-                return true;
-            }
-            return false;
-        }
+		public bool EliminarJugador(Jugador jugador) {
+			if (!System.IO.File.Exists(RutaXMLJugadores) || jugador == null)
+				return false;
 
-        public bool Desactivar()
-        {
-            try
-            {
-                XmlDocument xDoc = new XmlDocument();
-                xDoc.Load(RutaXMLCuentas);
+			XmlDocument xmlDoc = new XmlDocument();
+			xmlDoc.Load(RutaXMLJugadores);
+			var elemjugador = xmlDoc.SelectSingleNode("/Jugadores/Jugador['Id=" + jugador.Id + " Id-Cuenta=" + this.Email + "]");
+			if (elemjugador != null) {
+				elemjugador.ParentNode.RemoveChild(elemjugador);
+				xmlDoc.Save(RutaXMLJugadores);
+				return true;
+			}
+			return false;
+		}
 
-                var xCuenta = xDoc.SelectSingleNode("/Cuentas/Cuenta[Email = '" + this.Email + "' and Password = '" + this.Password + "']");
-                if (xCuenta == null)
-                {
-                    return false;
-                }
-                else {
-                    xCuenta.ParentNode.RemoveChild(xCuenta);
-                    xDoc.Save(RutaXMLCuentas);
-                }
-                return true;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
-    }
+		public bool Desactivar() {
+			try {
+				XmlDocument xmlDoc = new XmlDocument();
+				xmlDoc.Load(RutaXMLCuentas);
+
+				var xmlCuenta = xmlDoc.SelectSingleNode("/Cuentas/Cuenta[Email = '" + this.Email + "' and Password = '" + this.Password + "']");
+				if (xmlCuenta == null) {
+					return false;
+				} else {
+					xmlCuenta.ParentNode.RemoveChild(xmlCuenta);
+					xmlDoc.Save(RutaXMLCuentas);
+				}
+				return true;
+			} catch (Exception ex) {
+				return false;
+			}
+		}
+	}
 }
--- Ajedrez/Ajedrez.Models/Partida.cs
+++ Ajedrez/Ajedrez.Models/Partida.cs
@@ -32,63 +32,63 @@ namespace Ajedrez.Models {
 		}
 
 		public void Iniciar() {
-            Inicio = new DateTime();
-            for(int y = 0; y < 2; y++)
-            {
-                for(int x = 0; x < 4; x++)
-                {
-                    switch(x)
-                    {
-                        case 0:
-                            Tablero[x, y] = new Pieza();
-                            Tablero[x, y].Tipo = Tipo.TORRE;
-                            Tablero[4 + x, y] = new Pieza();
-                            Tablero[4 + x, y].Tipo = Tipo.TORRE;
-                            break;
-                        case 1:
-                            Tablero[x, y] = new Pieza();
-                            Tablero[x, y].Tipo = Tipo.CABALLO;
-                            Tablero[4 + x, y] = new Pieza();
-                            Tablero[4 + x, y].Tipo = Tipo.CABALLO;
-                            break;
-                        case 2:
-                            Tablero[x, y] = new Pieza();
-                            Tablero[x, y].Tipo = Tipo.ALFIL;
-                            Tablero[4 + x, y] = new Pieza();
-                            Tablero[4 + x, y].Tipo = Tipo.ALFIL;
-                            break;
-                        case 3:
-                            Tablero[x, y] = new Pieza();
-                            Tablero[x, y].Tipo = Tipo.REY;
-                            Tablero[4 + x, y] = new Pieza();
-                            Tablero[4 + x, y].Tipo = Tipo.REINA;
-                            break;
-                    }
-                }
-            }
+			Inicio = new DateTime();
+			for (int y = 0; y < 2; y++) {
+				for (int x = 0; x < 4; x++) {
+					switch (x) {
+						case 0:
+							Tablero[x, y] = new Pieza();
+							Tablero[x, y].Tipo = Tipo.TORRE;
+							Tablero[4 + x, y] = new Pieza();
+							Tablero[4 + x, y].Tipo = Tipo.TORRE;
+							break;
+						case 1:
+							Tablero[x, y] = new Pieza();
+							Tablero[x, y].Tipo = Tipo.CABALLO;
+							Tablero[4 + x, y] = new Pieza();
+							Tablero[4 + x, y].Tipo = Tipo.CABALLO;
+							break;
+						case 2:
+							Tablero[x, y] = new Pieza();
+							Tablero[x, y].Tipo = Tipo.ALFIL;
+							Tablero[4 + x, y] = new Pieza();
+							Tablero[4 + x, y].Tipo = Tipo.ALFIL;
+							break;
+						case 3:
+							Tablero[x, y] = new Pieza();
+							Tablero[x, y].Tipo = Tipo.REY;
+							Tablero[4 + x, y] = new Pieza();
+							Tablero[4 + x, y].Tipo = Tipo.REINA;
+							break;
+					}
+				}
+			}
 		}
 
 		public bool Jugar(Jugada jugada) {
-            if ( !(this.ValidarJugada(jugada)) ) return false;
-            var xy= jugada.Destino.Traducir();
-            var tmp= this.Tablero[xy[0], xy[1]];
+			if (!(this.ValidarJugada(jugada)))
+				return false;
+			var xy = jugada.Destino.Traducir();
+			var tmp = this.Tablero[xy[0], xy[1]];
 
-            this.Mover(jugada);
+			this.Mover(jugada);
 			this.GuardarJugada(jugada);
-			if( !(this.ValidarTablero()) ){
-                this.Retroceder();
-                this.Tablero[xy[0], xy[1]] = tmp;
-                return false;
-            }
+			if (!(this.ValidarTablero())) {
+				this.Retroceder();
+				this.Tablero[xy[0], xy[1]] = tmp;
+				return false;
+			}
 			return true;
 		}
 
 		private bool ValidarJugada(Jugada jugada) {
-            var cord = jugada.Origen.Traducir();
-            if (this.Tablero[cord[0], cord[1]].Color != this.Turno) return false;
-            cord = jugada.Destino.Traducir();
-            if(this.Tablero[cord[0], cord[1]].Color==this.Turno) return false;
-            return true;
+			var cord = jugada.Origen.Traducir();
+			if (this.Tablero[cord[0], cord[1]].Color != this.Turno)
+				return false;
+			cord = jugada.Destino.Traducir();
+			if (this.Tablero[cord[0], cord[1]].Color == this.Turno)
+				return false;
+			return true;
 		}
 
 		private void Mover(Jugada jugada) {
@@ -113,26 +113,22 @@ namespace Ajedrez.Models {
 			xDoc.Save(@"C:\utal-2016-1-mds-t2\jugadas[ID].xml");
 		}
 
-        private bool ValidarTablero()
-        {
-            for (int x = 0; x < Tablero.GetLength(0); x++)
-            {
-                for (int y = 0; y < Tablero.GetLength(1); y++)
-                {
-                    if (Tablero[x, y].Tipo.Equals(Tipo.REY))
-                    {
-                        /*
+		private bool ValidarTablero() {
+			for (int x = 0; x < Tablero.GetLength(0); x++) {
+				for (int y = 0; y < Tablero.GetLength(1); y++) {
+					if (Tablero[x, y].Tipo.Equals(Tipo.REY)) {
+						/*
                             Verificar si el rey puede moverse a su alrededor(JAQUE)
                             Si puede moverse retornamos TRUE, si no, el ciclo continua hasta terminar 
                             y arrojar FALSE.
                         */
-                    }
-                }
-            }
-            return false;
-        }
+					}
+				}
+			}
+			return false;
+		}
 
-        private void Retroceder() {
+		private void Retroceder() {
 			throw new NotImplementedException();
 		}
 
