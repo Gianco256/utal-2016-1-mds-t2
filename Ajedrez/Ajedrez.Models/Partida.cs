650054368881df1ced3c97f326e07f8558940a7f
Gianfranco <gianco256@gmail.com>
2016-06-11 22:45:26 -0400

    Partida.ValidarJugada(...) implementada

--- Ajedrez/Ajedrez.Models/Partida.cs
+++ Ajedrez/Ajedrez.Models/Partida.cs
@@ -81,17 +81,75 @@ namespace Ajedrez.Models {
 			return true;
 		}
 
-		private bool ValidarJugada(Jugada jugada) {
-			var cord = jugada.Origen.Traducir();
-			if (this.Tablero[cord[0], cord[1]].Color != this.Turno)
-				return false;
-			cord = jugada.Destino.Traducir();
-			if (this.Tablero[cord[0], cord[1]].Color == this.Turno)
-				return false;
-			return true;
-		}
+        private bool ValidarJugada(Jugada jugada)
+        {
+            //valida que el espacio de llegada este disponible o ocupado por una pieza rival
+            var cordLlegada = jugada.Destino.Traducir();
+            if (this.Tablero[cordLlegada[0], cordLlegada[1]] != null &&
+                this.Tablero[cordLlegada[0], cordLlegada[1]].Color == this.Turno) return false;
 
-		private void Mover(Jugada jugada) {
+            //valida que la posicion este ocupada por una pieza del turno actual
+            var cordInicio = jugada.Origen.Traducir();
+            var piezaEnMano = this.Tablero[cordInicio[0], cordInicio[1]];
+            if (piezaEnMano == null || piezaEnMano.Color != this.Turno) return false;
+
+            //Valida que el movimiento pueda ser efectuado por la pieza
+            var dX = cordInicio[0] - cordLlegada[0];
+            var dY = cordInicio[1] - cordLlegada[1];
+            switch (piezaEnMano.Tipo)
+            {
+                case Tipo.PEON:
+                    //comprueba que el movimiento sea hacia adelante
+                    //TODO: ratificar comprobaciones con el llenado del tablero
+                    if (piezaEnMano.Color == Color.BLANCO){
+                        if(dX<=0) return false;
+                    }else{
+                        if (dX>=0) return false;
+                    }
+
+                    if (Math.Abs(dY) == 1)
+                    {
+                        //posiblemente come
+                        if (this.Tablero[cordLlegada[0], cordLlegada[1]] == null) return false;
+                    }
+                    else if (Math.Abs(dY) != 0) return false;
+                    
+                    if (Math.Abs(dX) == 2) {
+                        if (cordInicio[0] != 1 || cordInicio[0] != 6) return false;
+                    } else if (Math.Abs(dX) != 1) return false;
+                    break;
+                case Tipo.TORRE:
+                    if (dX != 0 && dY != 0) return false;
+                    break;
+                case Tipo.CABALLO:
+                    // el unico que se comprueba diferente
+                    if (Math.Abs(dY) == 1 && Math.Abs(dX) == 3) return true;
+                    if (Math.Abs(dX) == 1 && Math.Abs(dY) == 3) return true;
+                    return false;
+                case Tipo.ALFIL:
+                    if (Math.Abs(dX) != Math.Abs(dY)) return false;
+                    break;
+                case Tipo.REY:
+                    if (Math.Abs(dX)>1 || Math.Abs(dY)>1) return false;
+                    break;
+                case Tipo.REINA:
+                    if ((dX != 0 && dY != 0) || Math.Abs(dX) != Math.Abs(dY)) return false;
+                    break;
+                default:
+                    return false;
+            }
+            cordInicio[0] += dX;
+            cordInicio[1] += dY;
+            //comprueba que el camino este libre
+            for (; cordInicio[0] != cordLlegada[0] || cordInicio[0] != cordLlegada[0];
+                    cordInicio[0] += dX, cordInicio[1] += dY)
+            {
+                if (this.Tablero[cordInicio[0], cordInicio[1]] != null) return false;
+            }
+            return true;
+        }
+
+        private void Mover(Jugada jugada) {
 			Pieza temp = this.Tablero[jugada.Origen.Traducir()[0], jugada.Origen.Traducir()[1]];
 			this.Tablero[jugada.Origen.Traducir()[0], jugada.Origen.Traducir()[1]] = null;
 			this.Tablero[jugada.Destino.Traducir()[0], jugada.Destino.Traducir()[1]] = temp;
