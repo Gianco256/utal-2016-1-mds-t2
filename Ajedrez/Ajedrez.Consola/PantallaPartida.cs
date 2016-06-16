8d61c435bb3e92bd92272307b751f75e09d7a496
eortizutalca <eortiz@utalca.cl>
2016-06-16 12:05:08 -0400

    Crea pantalla de partidas

---
+++ Ajedrez/Ajedrez.Consola/PantallaPartida.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajedrez.Consola
+{
+    public class PantallaPartida
+    {
+        public static void Desafiar(Models.Jugador j)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Detalle(Models.Partida p)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
